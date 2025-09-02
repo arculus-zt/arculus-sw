3e3d89437a359db01f8710ea1de23ce8c125d64e
Bishnu Bhusal <bishnu.bhusal111@gmail.com>
2025-09-02 11:31:56 -0500

    controller logic updated

--- arculus-gcs-node/services/missionService.js
+++ arculus-gcs-node/services/missionService.js
@@ -9,6 +9,10 @@ const crypto = require('crypto');
 const cryptSecret = fs.readFileSync('configs/ENCRYPTION_SECRET.txt', 'utf8').trim();
 const certConfig = JSON.parse(fs.readFileSync('configs/cert.json'));
 
+const NOAUTH = 'NOAUTH';
+const AUTH_TOKEN_BASED = 'AUTH_TOKEN_BASED';
+const AUTH_CERT_BASED = 'AUTH_CERT_BASED';
+
 // Define global variables
 let gcX = null;
 let gcY = null;
@@ -692,9 +696,9 @@ exports.executeStealthyReconAndResupply = (req, res) => {
                         ipCache[supplyDrone] = supplyDroneIP;
                         
                         const deviceMap = {
-                        [relayDrone]: relayDroneIP,
-                        [surveillanceDrone]: surveillanceDroneIP,
-                        [supplyDrone]: supplyDroneIP,
+                        [relayDroneIP]: [relayDrone, NOAUTH],
+                        [surveillanceDroneIP]: [surveillanceDrone, AUTH_CERT_BASED],
+                        [supplyDroneIP]: [supplyDrone, AUTH_TOKEN_BASED],
                         };
 
 
